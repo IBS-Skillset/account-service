6d1e531cec366fac910886e3b2d00bd0436a9371
Abhishek Ravindran <a-10749@ibsplc.com>
2022-10-19 15:26:22 +0530

    postgreSQL configuration

--- src/main/java/org/skillset/mystays/Account.java
+++ src/main/java/org/skillset/mystays/Account.java
@@ -1,5 +1,15 @@
 package org.skillset.mystays;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Account {
 
     private Long id;
@@ -8,61 +18,4 @@ public class Account {
     private String lastName;
     private String password;
     private String phoneNumber;
-
-    public Account(Long id, String email, String firstName, String lastName, String password, String phoneNumber) {
-        this.id = id;
-        this.email = email;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.password = password;
-        this.phoneNumber = phoneNumber;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
