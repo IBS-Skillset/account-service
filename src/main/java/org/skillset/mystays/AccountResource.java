e5adb02212fceb209662a9c3dc6cdb8d11287c83
Abhishek Ravindran <a-10749@ibsplc.com>
2022-10-20 08:57:25 +0530

    removing cors config

--- src/main/java/org/skillset/mystays/AccountResource.java
+++ src/main/java/org/skillset/mystays/AccountResource.java
@@ -14,7 +14,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 
-@Path("account")
+@Path("account/api")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AccountResource {
