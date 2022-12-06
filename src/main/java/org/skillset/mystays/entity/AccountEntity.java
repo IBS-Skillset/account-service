2365c968944ea0477d2e0af069ff68ffccf8d4a6
nshibu <v-nshibu@expedia.com>
2022-12-06 13:36:44 +0530

    [UP][TECH-544]User Profile Details

--- src/main/java/org/skillset/mystays/entity/AccountEntity.java
+++ src/main/java/org/skillset/mystays/entity/AccountEntity.java
@@ -3,15 +3,23 @@ package org.skillset.mystays.entity;
 import javax.persistence.*;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import lombok.Data;
 
+import java.util.List;
 
+@Data
 @Entity
 @Table(name = "USERS")
 public class AccountEntity extends PanacheEntityBase {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     public long id;
+
+    @OneToMany(mappedBy="account", cascade = CascadeType.ALL)
+    public List<UserAddress>  address;
+
     @Column(name = "first_name")
     public String firstName;
     @Column(name = "last_name")
@@ -22,6 +30,7 @@ public class AccountEntity extends PanacheEntityBase {
     public String email;
     public String role="USER";
     public boolean enabled = true;
-
     public String phone;
+
+
 }
