75d5cb85b66e5430b32a71afb7cb14a1cd7d7fce
Abhishek Ravindran <v-aravindran@expediagroup.com>
2022-12-15 11:42:53 +0530

    my trips integration

--- src/main/java/org/skillset/mystays/entity/UserAddress.java
+++ src/main/java/org/skillset/mystays/entity/UserAddress.java
@@ -1,25 +1,27 @@
 package org.skillset.mystays.entity;
 
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import lombok.Data;
-import javax.persistence.Entity;
-import javax.persistence.Column;
-import javax.persistence.ManyToOne;
-import javax.persistence.CascadeType;
+
+import javax.persistence.*;
 
 @Data
 @Entity
+@Table(name = "user_address")
 public class UserAddress extends PanacheEntity {
     @Column(name = "type")
-    public String type="permanent";
+    public String type = "permanent";
     @Column(name = "address")
     public String address;
     @Column(name = "city")
-    public String city ="New york";
+    public String city = "New york";
     @Column(name = "country")
-    public String country="USA";
+    public String country = "USA";
     @Column(name = "zipcode")
     public String zipcode;
-    @ManyToOne(cascade = CascadeType.ALL)
-    public  AccountEntity user;
+    @ManyToOne(cascade = CascadeType.MERGE)
+    public AccountEntity user;
 }
+
+
