025410d0933f56d7c5123cef60306aeb731a9349
Babu Kalarikkal <kalarikkalbabu@gmail.com>
2023-04-19 16:54:54 +0530

    Update AccountController.java

--- src/main/java/org/skillset/mystays/controller/AccountController.java
+++ src/main/java/org/skillset/mystays/controller/AccountController.java
@@ -13,7 +13,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("account/api")
+@Path("api-gateway/account/api")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AccountController {
