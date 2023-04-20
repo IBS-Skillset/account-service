e203776871a9753aea6523695923318073afe1ea
Babu Kalarikkal <kalarikkalbabu@gmail.com>
2023-04-20 12:58:12 +0530

    Update AccountController.java remove api-gateway url

--- src/main/java/org/skillset/mystays/controller/AccountController.java
+++ src/main/java/org/skillset/mystays/controller/AccountController.java
@@ -13,7 +13,7 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("api-gateway/account/api")
+@Path("account/api")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class AccountController {
